71bcf664aacc5a077233629d063dd2dbc216bdcc
farique <fariqueparammel@gmail.com>
2025-05-21 13:27:05 +0530

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,91 @@
+// Service Worker for Weather App
+
+const CACHE_NAME = "weather-app-v2";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./assets/index-7d22435c.css",
+  "./assets/index-c23ae31b.js",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+];
+
+// Install a service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Cache and return requests
+self.addEventListener("fetch", (event) => {
+  // Skip cross-origin requests
+  if (
+    !event.request.url.startsWith(self.location.origin) &&
+    !event.request.url.startsWith("https://api.openweathermap.org")
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Cache hit - return response
+      if (response) {
+        return response;
+      }
+      return fetch(event.request)
+        .then((response) => {
+          // Check if we received a valid response
+          if (
+            !response ||
+            response.status !== 200 ||
+            response.type !== "basic"
+          ) {
+            return response;
+          }
+
+          // Clone the response
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME).then((cache) => {
+            // Don't cache API requests
+            if (!event.request.url.includes("api.openweathermap.org")) {
+              cache.put(event.request, responseToCache);
+            }
+          });
+
+          return response;
+        })
+        .catch((error) => {
+          console.log("Fetch failed; returning offline page instead.", error);
+          // You could return a custom offline page here
+        });
+    })
+  );
+});
+
+// Update a service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
